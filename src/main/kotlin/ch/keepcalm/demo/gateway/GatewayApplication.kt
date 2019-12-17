0c0df3b19a5eed51f82987f14bc43affb959ed2d
Marcel Widmer <marzelwidmer@gmail.com>
2019-12-17 22:07:02 +0100

    Work on Routing with Kubernetes

--- src/main/kotlin/ch/keepcalm/demo/gateway/GatewayApplication.kt
+++ src/main/kotlin/ch/keepcalm/demo/gateway/GatewayApplication.kt
@@ -2,12 +2,14 @@ package ch.keepcalm.demo.gateway
 
 import ch.sbb.esta.openshift.gracefullshutdown.GracefulshutdownSpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.annotation.PostConstruct
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayApplication {
     @PostConstruct
     fun init() {
