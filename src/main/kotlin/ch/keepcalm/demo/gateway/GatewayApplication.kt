0e8486c05eebdb59237c1fee317f541e873bba33
Marcel Widmer <marzelwidmer@gmail.com>
2020-03-21 16:43:41 +0100

    CleanUp tokens in RestClient

--- src/main/kotlin/ch/keepcalm/demo/gateway/GatewayApplication.kt
+++ src/main/kotlin/ch/keepcalm/demo/gateway/GatewayApplication.kt
@@ -30,80 +30,9 @@ import java.util.function.Consumer
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 class GatewayApplication {
     // This definition is used to get Intellij happy
-//    @Bean
-//    fun reactiveResilience4JCircuitBreakerFactory ( )= ReactiveResilience4JCircuitBreakerFactory()
+    @Bean
+    fun reactiveResilience4JCircuitBreakerFactory() = ReactiveResilience4JCircuitBreakerFactory()
 }
-
-//{
-//To enable circuit breaker built on top of Resilience4J we need to declare Customizer bean that is
-// passed a ReactiveResilience4JCircuitBreakerFactory. The very simple configuration contains default circuit breaker
-// settings and and defines timeout duration using TimeLimiterConfig.
-// For the first test I decided to set 200 milliseconds timeout.
-//    @Bean
-//    fun defaultCustomizer(): Customizer<ReactiveResilience4JCircuitBreakerFactory>? {
-//        return Customizer { factory: ReactiveResilience4JCircuitBreakerFactory ->
-//            factory.configureDefault { id: String? ->
-//                Resilience4JConfigBuilder(id)
-//                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-//                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
-//                        .build()
-//            }
-//        }
-//    }
-
-// The property slidingWindowSize defines how many outcome calls has to be recorded when a circuit breaker is closed.
-// Assuming we have the same test endpoints what will happen if we change this value to 10 as shown below?
-//    @Bean
-//    fun defaultCustomizerWithSlidingWindowSize(): Customizer<ReactiveResilience4JCircuitBreakerFactory>? {
-//        return Customizer { factory: ReactiveResilience4JCircuitBreakerFactory ->
-//            factory.configureDefault { id: String? ->
-//                Resilience4JConfigBuilder(id)
-//                        .circuitBreakerConfig(CircuitBreakerConfig.custom()
-//                                .slidingWindowSize(10)
-//                                .build())
-//                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build()).build()
-//            }
-//        }
-//    }
-
-
-//    @Bean
-//    fun defaultCustomizerFailureRateThreshold(): Customizer<ReactiveResilience4JCircuitBreakerFactory>? {
-//        return Customizer { factory: ReactiveResilience4JCircuitBreakerFactory ->
-//            factory.configureDefault { id: String? ->
-//                Resilience4JConfigBuilder(id)
-//                        .circuitBreakerConfig(CircuitBreakerConfig.custom()
-//                                .slidingWindowSize(10)
-//                                .failureRateThreshold(66.6f)
-//                                .build())
-//                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build()).build()
-//            }
-//        }
-//    }
-
-//@Bean
-//fun slowCusomtizer(): Customizer<ReactiveResilience4JCircuitBreakerFactory>? {
-//    return Customizer { factory: ReactiveResilience4JCircuitBreakerFactory ->
-//        factory.configure(Consumer { builder: Resilience4JConfigBuilder ->
-//            builder
-//                    .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build())
-//                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-//        }, "slow", "slowflux")
-//        factory.addCircuitBreakerCustomizer(Customizer { circuitBreaker: CircuitBreaker ->
-//            circuitBreaker.eventPublisher
-//                    .onError(normalFluxErrorConsumer).onSuccess(normalFluxSuccessConsumer)
-//        }, "normalflux")
-//    }
-//}
-//    @Bean
-//    fun circuitBreakerConfig() = CircuitBreakerConfig.custom()
-//            .minimumNumberOfCalls(2)
-//            .failureRateThreshold(50F)
-//            .waitDurationInOpenState(Duration.ofMillis(1000))
-//            .permittedNumberOfCallsInHalfOpenState(2)
-//            .slidingWindowSize(2)
-//            .build()
-//}
 
 
 fun main(args: Array<String>) {
--- src/main/kotlin/ch/keepcalm/demo/gateway/LivenessProbe.kt
+++ src/main/kotlin/ch/keepcalm/demo/gateway/LivenessProbe.kt
@@ -2,14 +2,10 @@ package ch.keepcalm.demo.gateway
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 
 @RestController
 class LivenessProbe {
     @GetMapping(value = ["/alive"])
-    fun alive(): String {
-        System.out.println("Date in Europe/Zurich: ${Date().toString()}")
-        return "ok"
-    }
+    fun alive() = "ok"
 }
